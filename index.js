4e1124583792a595484d709e2c8fb7408a495f76
Iron Man <narendrannirmal6753@gmail.com>
2024-02-19 18:29:40 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+//so you came here....
+//here's a cookie for you 🍪
+
+const pinterestdl = require('./lib/Pinterest/pinterest.js');
+const genshinCH = require('./lib/Genshin/Character.js');
+
+
+module.exports = {
+  pinterestdl,
+  genshinCH
+};
